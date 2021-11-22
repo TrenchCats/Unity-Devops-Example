bc151a8ca9bcac1896c3d14e07c60ac3c6cbfcbf
AceDroo <44382889+MitchDroo@users.noreply.github.com>
2021-11-22 20:36:09 +1100

    ci: Add Code Coverage Package

---
+++ Test/Assets/Scripts/Clock.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock {
+    public int Hour { get; set; }
+    public int Minute { get; set; }
+
+    private int timer;
+
+    public Clock(int startingHour, int startingMinute)
+    {
+        this.Hour = startingHour;
+        this.Minute = startingMinute;
+    }
+    
+    public static Clock operator +(Clock a, Clock b) {
+        int hour = a.Hour + b.Hour;
+        int minute = a.Minute + b.Minute;
+
+        if (minute >= 60)
+        {
+            hour += (minute / 60);
+            minute %= 60;
+        } 
+        else if (minute < 0)
+        {
+            hour = (minute / 60);
+            minute %= 60;
+            minute = 60 - minute;
+        }
+
+        return new Clock(hour, minute);
+    }
+
+    public static bool operator ==(Clock a, Clock b)
+    {
+        return a.Hour == b.Hour && a.Minute == b.Minute;
+    }
+
+    public static bool operator !=(Clock a, Clock b)
+    {
+        return a.Hour != b.Hour || a.Minute != b.Minute;
+    }
+
+    public string GetTimeString()
+    {
+        return $"{ Hour }:{ Minute }";
+    }
+}
